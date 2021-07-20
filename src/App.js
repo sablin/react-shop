ec77eed01953785270a9aa4605b65ec2a98fc5d4
sablin <sablin.irk@gmail.com>
2021-07-20 16:19:10 +0800

    Drawer and search

--- src/App.js
+++ src/App.js
@@ -2,7 +2,54 @@
   function App() {
   return (
     <div className="wrapper clear">
- 
+      
+      <div className="overlay">
+        <div className="drawer">
+    <h2 className="mb-20">Корзина</h2>
+    <div className="items">
+
+    <div className="mb-20 cartItem d-flex align-center">
+        <div  style={{backgroundImage: 'url(/img/pic/1.png)'}} className="cartItemImg">
+        <img className="mr-20" width={70} height={70} src="/img/pic/1.png" alt="Sneakers" />
+      
+        </div>
+         <div className="mr-20 flex">
+          <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+          <b>12 999 руб.</b>
+        </div>
+        <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+      </div>
+
+      <div className="mb-20 cartItem d-flex align-center">
+        <div  style={{backgroundImage: 'url(/img/pic/1.png)'}} className="cartItemImg">
+        <img className="mr-20" width={70} height={70} src="/img/pic/1.png" alt="Sneakers" />
+      
+        </div>
+         <div className="mr-20 flex">
+          <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+          <b>12 999 руб.</b>
+        </div>
+        <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+      </div>
+    </div>
+
+      <ul className="cartTotalBlock">
+        <li className="d-flex">
+          <span>Итого:</span>
+          <div></div>
+          <b>21468 руб.</b>
+        </li>
+        <li  className="d-flex">
+          <span>Налог 5%:</span>
+          <div></div>
+          <b>1074 руб.</b>
+        </li>
+      </ul>
+
+        </div>
+      </div>
+
+
       <header className="d-flex justify-between align-center">
         <div className="d-flex align-center">
         <img width={40} height={40} src="/img/logo.png" alt="Логотип"/>
@@ -25,10 +72,21 @@
       </ul>
       </header>
       <main className="content p-40">
-      <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center justify-between mb-40">
+        <h1>Все кроссовки</h1>
+        <div className="d-flex search-block">
+          <img src="/img/search.svg" alt="Search" />
+          <input type="text" placeholder="Поиск..." />
+        </div>
+        </div>
+  
 
       <div className="d-flex">
       <div className="card">
+        <div className="favorite">
+        <img src="/img/heart-unliked.svg" alt="Unliked" />
+          </div>
+        
           <img width={133} height={112} src="img/pic/1.png"/>
           <h5>Мужские Кроссовки Nike Air Max 270</h5>
           <div className="d-flex justify-between align-center">
